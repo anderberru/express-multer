7fb7888ec0d3f17cf28a4818e3081e286450b465
Ander Berruezo Aguirre <berru0429@gmail.com>
2023-12-11 16:56:18 +0100

    kaixo

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,39 @@
 var express = require('express');
 var router = express.Router();
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+//const upload = multer({ dest: 'uploads/' })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      const name = file.originalname.split('.')
+      cb(null, name[0] + "-" + uniqueSuffix + "." + name[1])
+    }
+  })
+  
+
+
+function fileFilter (req, file, cb) {
+
+    // The function should call `cb` with a boolean
+    // to indicate if the file should be accepted
+    if (file.originalname.split('.')[1] === "png" || file.originalname.split('.')[1] === "jpg"){
+        // To reject this file pass `false`, like so:
+        cb(null, true)
+    } else {
+        // To accept the file pass `true`, like so:
+        cb(new Error('Error: File must be a png'), false)
+    }
+  
+    // You can always pass an error if something goes wrong:
+    //cb(new Error('I don\'t have a clue!'))
+  
+}
+
+const upload = multer({ storage: storage, fileFilter: fileFilter, limits:{fileSize: 2*1024*1024} })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
